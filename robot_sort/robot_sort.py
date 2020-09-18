41cb155968a7cc64d128f7937b3383f83b510c4d
charlupuhh <62491397+charlupuhh@users.noreply.github.com>
2020-09-18 09:52:25 -0700

    mvp

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,9 +96,18 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
-
+        while self.can_move_right():
+            self.swap_item()
+            # traverse the list to find the smallest item
+            while self.can_move_right():
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
+            # move back to where you got your first item and swap it
+            while self.compare_item() != None:
+                self.move_left()
+            self.swap_item()
+            self.move_right()
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
